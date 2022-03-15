02709e2f10b96ba1bb67618f9ed12803da472bec
Ronaldo-Bianco-Caiena <ronaldo.bianco@caiena.net>
2022-03-15 13:17:49 -0300

    project settings

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/ateliware'));
+app.use(express.static('/dist/ateliware'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/ateliware/index.html'));});
+res.sendFile(path.join('/dist/ateliware/index.html'));});
 app.listen(process.env.PORT || 8080);
